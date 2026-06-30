c1f0380fee2a5d008f0fa00972815214aa7800c6
Priyanshu Sinha <priyanshu.sinha12345@gmail.com>
2026-06-30 11:33:20 +0530

    updated

--- day7/script.js
+++ day7/script.js
@@ -4,7 +4,6 @@
 //   console.log("This function runs immediately");
 // })();
 
-
 //* Callback function
 
 // function greet(uname, callback) {
@@ -15,7 +14,6 @@
 //     console.log("Greeting is done");
 // }
 // greet("Ajay", done);
-
 
 //* Callback Hell
 
@@ -45,25 +43,125 @@
 //? 5. Nested structure keeps growing
 //? Solution = Promisses
 
-//* Promisses - A promise is an object that represent the eventual completion or failure of an asyncronous operation
+//* Promises - A promise is an object that represent the eventual completion or failure of an asyncronous operation
 
-//? Promises States: Pending, Fullfiled, Rejected
+//? Promises States: Pending, Fullfilled, Rejected
 
-const myPromise = new Promise((resolve, rejected) => {
-    let success = true;
-    if (success) {
-        resolve("Data received successfully!");
-    } else {
-        rejected("Something went wrong!");
-    }
-})
+// const myPromise = new Promise((resolve, rejected) => {
+//     let success = true;
+//     if (success) {
+//         resolve("Data received successfully!");
+//     } else {
+//         rejected("Something went wrong!");
+//     }
+// })
 
-// console.log(myPromise);
+// // console.log(myPromise);
 
-myPromise.then((result) => {
-    console.log(result);
-}).catch((error) => {
-    console.log(error);
-})
+// myPromise.then((result) => {
+//     console.log(result);
+// }).catch((error) => {
+//     console.log(error);
+// })
+
+// const download = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Download Complete!");
+//   }, 5000);
+// });
+
+// download.then((data) => {
+//   console.log(data);
+// });
+
+//* ---------------------------------------------
+
+//? Using Callback hell
+// loginUser(function (user) {
+//     getProfile(user.id, function (profile) {
+//         getPosts(profile.id, function (posts) {
+//             console.log(posts);
+//       })
+//   });
+// });
+
+//? Using Promises
+//? Promise chaining
+// loginUser()
+//     .then((user) => { getProfile(user.id) })
+//     .then((profile) => { getPosts(profile.id) })
+//     .then((posts) => console.log(posts))
+//     .catch((error) => console.log(error))
+//     .finally(() => console.log("Closing Promise"));
+
+//* ---------------------------------------------
+
+// Promise.resolve("This has been resolved")
+//     .then(console.log);
+
+// Promise.reject("Wrong Password").catch(console.log);
 
 
+//? Promise.all - Runs multiple promises simultaneously
+// const p1 = Promise.resolve("A");
+// const p2 = Promise.resolve("B");
+// const p3 = Promise.resolve("C");
+
+// Promise.all([p1, p2, p3])
+//     .then((result) => {
+//         console.log(result);
+//     })
+//     .catch((error) => { console.log(`Error: ${error}`); });
+    
+//? Promise.race - returns whichever Promise settles first (resolve or reject)
+// const p1 = new Promise((resolve) => {
+//     setTimeout(() => {
+//         resolve("First")
+//     }, 3000);
+// });
+
+// const p2 = new Promise((resolve) => {
+//   setTimeout(() => {
+//     resolve("Second");
+//   }, 2000);
+// });
+
+// Promise.race([p1, p2])
+//     .then(console.log);
+
+
+//? Promise.allSettled - waits for every promise, regardless of success or failure
+// Promise.allSettled([
+//     Promise.resolve("A"),
+//     Promise.reject("Error")
+// ])
+//     .then(console.log);
+
+
+//? Promise.any - returns the first fullfilled Promise, unless all promises are rejected
+// Promise.any([
+//     Promise.reject("Error"),
+//     Promise.resolve("B"),
+//     Promise.resolve("A")
+// ])
+//     .then(console.log);
+
+
+//* Fetching API
+
+// const title = document.querySelector('#postTitle');
+// const id = document.querySelector('#postId');
+// const body = document.querySelector('#postBody');
+// fetch("https://jsonplaceholder.typicode.com/posts/1")
+//     .then(res => res.json())
+//     .then(post => {
+//         title.innerText = post.title;
+//         id.innerText = post.id;
+//         body.innerText = post.body;
+//     })
+//     .catch(error => {
+//         console.log("jsonplaceholder.typecode.com is not responding!\nError:", error);
+//     })
+
+
+//* 
